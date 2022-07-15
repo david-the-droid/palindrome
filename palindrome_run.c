036f923331b11002472106ba348e88e70811f41e
David Anderson <david.anderson@apollo-fire.com>
2022-07-15 14:37:17 +0100

    Started development of project, committing code with two for loops for palindrome, first loop removes spaces from the string, second loop converts upper case characters to lower case, third loop present not yet implemented

--- palindrome_run.c
+++ palindrome_run.c
@@ -1,2 +1,42 @@
-// development file for C code
-// test commit
+#include <stdio.h>
+
+int main() {
+   
+   char test_string[9] = "Top spot";
+   char result_string[9];
+   int non_space_character_inc = 0;
+   
+   // want to remove spaces
+
+   for (int index = 0; index<sizeof(test_string); index++)
+   {
+      if (test_string[index] != ' ')
+      {
+         result_string[non_space_character_inc] = test_string[index]; 
+         non_space_character_inc++;
+      }
+
+   }
+   printf("Print result_string %s\n", result_string);
+
+   // want to remove captial letters
+
+   for (int index = 0; index<sizeof(result_string); index++)
+   {
+      if(result_string[index] >= 65 && result_string[index] <= 90)
+      {
+         result_string[index] = result_string[index] + 32; // offset ascii by 32 to shift upper to lower case
+      }
+   }
+
+   printf("Print result_string %s\n", result_string);
+   // now want to reverse array and compare
+
+   for(int index = 0; index<sizeof(result_string); index++)
+   {
+      
+   }
+
+
+   return 0;
+}
