6b3158e64425898d48c845a186f818874fbc4e24
David Anderson <david.anderson@apollo-fire.com>
2022-07-17 22:10:30 +0100

    First working revision of Palindrome complete and tested, changes made involved: 1) Improving variable names 2) Improving char initilisation 3) Using built-in C functions to perform strcpy & strcmp 4) Fixed some bugs with the memory management of the arrays

--- palindrome_run.c
+++ palindrome_run.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 // lets define a struct
 
-#define INPUT_LEN (9u)
+#define INPUT_LEN (20u)
 
 int main() {
-   
-   char test_string[INPUT_LEN] = "Top spot";
-   char result_string[INPUT_LEN];
-   int non_space_character_inc = 0;
+
+   char input_string[INPUT_LEN] = " ";   // Input string
+   char result_string[INPUT_LEN] = " ";  // De-capitalised, de-spaced and reversed to compare for palindrome
+   char comparison_string[INPUT_LEN];    // comparison char array for lower case comparison copy
+   int non_space_character_inc = 0;      // Counts N.O of non space characters to form index for result_string
+
+   // want to get user input (not yet implemented)
+
+   strcpy(input_string, "Top Spot");
 
    // want to remove spaces
-
+   
    for (int index = 0; index<INPUT_LEN; index++)
    {
-      if (test_string[index] != ' ')
+      if (input_string[index] != ' ')
       {
-         result_string[non_space_character_inc] = test_string[index]; 
+         result_string[non_space_character_inc] = input_string[index]; 
          non_space_character_inc++;
       }
 
@@ -24,7 +30,7 @@ int main() {
    printf("Print result_string %s\n", result_string);
 
    // want to remove captial letters
-
+   
    for (int index = 0; index<INPUT_LEN; index++)
    {
       if(result_string[index] >= 65 && result_string[index] <= 90)
@@ -33,14 +39,27 @@ int main() {
       }
    }
 
+   // need a strcopy here to form a comparison string once the result_string is reversed
+
+   strcpy(comparison_string, result_string);
+
    printf("Print result_string %s\n", result_string);
    // now want to reverse array and compare
 
-   for(int index = 0; index<INPUT_LEN; index++)
-   {
-      
-   }
+   strrev(result_string);
 
+   printf("Final result is: %s\n", result_string);
+
+   // now need comparison
+
+   if (strcmp(result_string, comparison_string) == 0)
+   {
+      printf("True\n");
+   }
+   else
+   {
+      printf("False\n");
+   }
 
    return 0;
 }
